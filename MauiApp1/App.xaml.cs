40c99ab71124bb19d3e56479b096a90854503ffa
Michael Bond <codemonkey8510@icloud.com>
2021-11-09 23:47:25 -0500

    Add project files.

---
+++ MauiApp1/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Application = Microsoft.Maui.Controls.Application;
+
+namespace MauiApp1;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+        MainPage = new MainPage();
+    }
+}
